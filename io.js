712c1b430302470eba365376a2c0d29974d6a370
James Costello <jcostello10@gmail.com>
2016-11-23 12:24:52 -0800

    changes to controller/io/app

--- io.js
+++ io.js
@@ -1,13 +1,19 @@
 // io.js
 
 var io = require('socket.io')();
+var questionLoad = require('./config/question')
 
 // Listen for new connections from clients (socket)
 io.on('connection', function (socket) {
 
+    socket.on('new-question', function (data){
+      io.emit('update-question', data)
+    })
+
   socket.on('add-message', function (data) {
       io.emit('add-message', data);
     });
+
 
 });
 
